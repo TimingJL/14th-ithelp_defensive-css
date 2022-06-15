b945131c059f01982a07c01a2dfddcbaad3395dc
taiming <eefozeo@gmail.com>
2022-06-15 16:29:14 +0800

    Add Day04 page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {
 } from "react-router-dom";
 import Home from './pages/Home';
 import Day03 from './pages/Day03_title-and-icon';
+import Day04 from './pages/Day04_buttons';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/day03" element={<Day03 />} />
+      <Route path="/day04" element={<Day04 />} />
     </Routes>
   );
 }
---
+++ src/pages/Day04_buttons/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Day04 = () => {
+  return (
+    <div>Day04</div>
+  );
+};
+
+export default Day04;
