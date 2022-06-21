c8ea0cf62431a10f7e27420559dd17079a890073
taiming <eefozeo@gmail.com>
2022-06-21 12:43:38 +0800

    Add Day06 route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Day03 from './pages/Day03_title-and-icon';
 import Day04 from './pages/Day04_buttons';
 import Day05 from './pages/Day05_category_tag';
+import Day06 from './pages/Day06_scrollable_element';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/day03" element={<Day03 />} />
       <Route path="/day04" element={<Day04 />} />
       <Route path="/day05" element={<Day05 />} />
+      <Route path="/day06" element={<Day06 />} />
     </Routes>
   );
 }
